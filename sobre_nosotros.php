1a6f54ebcadd6696c3fd54a8f847d101d5fda0f3
hector pacheco <136692126+istito1000@users.noreply.github.com>
2023-11-17 15:19:47 -0300

    Initial commit

---
+++ sobre_nosotros.php
@@ -0,0 +1,59 @@
+<?php
+require 'config/config.php';
+require 'config/database.php';
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nosotros - JoseGasam</title>
+    <link rel="stylesheet" href="css/estilo_sobre_nosotros.css">
+
+</head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login- JoseGasam</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="script.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./styles.css">
+    <style>
+        body{
+            background:#ffe259;
+            background:linear-gradient(to right, #ff8001, #fbff02);
+        
+        }
+
+        .bg{
+            background-image:url(img/prueba1.jpg) ;
+            background-position: center center; 
+        }
+    </style>
+</head>
+
+<body>
+<?php include 'menu.php';?>
+
+    <div class="container">
+        <div class="row m-">
+            <div class="col"></div>
+            <div class="col"></div>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2>Sobre Nosotros</h2>
+    </div>
+
+    <footer>
+        <p>&copy; 2023 Tienda de Balones de Gas Abastible</p>
+    </footer>
+    
+</body>
+</html>
+
